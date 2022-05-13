b9839e49a78739eed072f0582a410d54dda3f3f6
Islam Naasani <islamnaasani@gmail.com>
2022-05-13 22:57:49 +0300

    add skip and limit for posts

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import PostI from "../interfaces/Post";
 import { fetchAPI } from "../utils/fetchAPI";
 import Author from "./Author";
@@ -8,10 +9,17 @@ import PostedAt from "./PostedAt";
 import "./style/Feed.scss";
 function Feed() {
     const [posts, setPosts] = useState<Array<PostI>>([]);
-
-    const getFeed = async () => {
-        const res = await fetchAPI("posts/feed");
-        setPosts(res.body.posts);
+    const [skip, setSkip] = useState(0);
+    const [isThereMorePosts, setIsThereMorePosts] = useState(true);
+    const limit = 10;
+    const getFeed = async (skip: number) => {
+        const res = await fetchAPI(`posts/feed/?skip=${skip}&limit=${limit}`);
+        setPosts((posts) => {
+            return [...posts, ...res.body.posts];
+        });
+        if (res.body.posts.length < limit) {
+            setIsThereMorePosts(false);
+        }
     };
     const postDeletedCallback = (postId: string) => {
         setPosts((posts) => {
@@ -20,12 +28,18 @@ function Feed() {
             });
         });
     };
+    const loadMorePosts = () => {
+        setSkip((skip) => {
+            return skip + limit;
+        });
+    };
     useEffect(() => {
-        getFeed();
-    }, []);
+        getFeed(skip);
+    }, [skip]);
     if (!posts.length) return <Loading />;
     return (
         <section className="feed">
+            <Link to="/new-post">Add post</Link>
             {posts.map((post) => {
                 return (
                     <PostCard
@@ -41,6 +55,7 @@ function Feed() {
                     </PostCard>
                 );
             })}
+            {isThereMorePosts && <button onClick={loadMorePosts}>Show more posts</button>}
         </section>
     );
 }
