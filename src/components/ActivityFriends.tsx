223a47456bafe6e47c31cd4699cb56e7f2548ad4
Islam Naasani <islamnaasani@gmail.com>
2022-05-31 21:41:09 +0300

    use FriendButtons in activityFriends

--- src/components/ActivityFriends.tsx
+++ src/components/ActivityFriends.tsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useListLoading from "../hooks/useListLoading";
-import { UserMini } from "../interfaces/User";
-import FriendList from "./FriendList";
+import { FriendI } from "../interfaces/User";
+import FriendCard from "./FriendCard";
 import Loading from "./Loading";
 
 function ActivityFriends() {
-    const [friends, setFriends] = useState<Array<UserMini>>();
+    const [friends, setFriends] = useState<Array<FriendI>>();
     const { userId } = useParams();
     const route = `users/${userId}/friends`;
     const {
         list,
         isThereMoreFromList: isThereMoreFriends,
         loadMoreFromList,
-    } = useListLoading<UserMini>(10, route, "friends");
+    } = useListLoading<FriendI>(10, route, "friends");
 
     useEffect(() => {
         setFriends(list);
@@ -21,7 +21,9 @@ function ActivityFriends() {
     if (!friends) return <Loading />;
     return (
         <section className="friends">
-            <FriendList friends={friends} />
+            {friends.map((friend) => {
+                return <FriendCard friend={friend} key={friend._id} />;
+            })}
             {isThereMoreFriends && (
                 <button className="load-more" onClick={loadMoreFromList}>
                     Load more friends
