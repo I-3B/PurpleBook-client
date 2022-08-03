3ba27709c2a6691a4485df3b74c82b2bd38d3daa
Islam Naasani <islamnaasani@gmail.com>
2022-08-04 00:32:16 +0300

    remove useless logout component

--- src/components/Logout.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router";
-import useAuth from "../hooks/useAuth";
-
-function Logout() {
-    const { logout } = useAuth();
-    logout();
-    return <Navigate to="/login" />;
-}
-export default Logout;
