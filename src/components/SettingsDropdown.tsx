792d00c46d51ac2ac5d5262320eab3f78665d42d
Islam Naasani <islamnaasani@gmail.com>
2022-08-18 22:32:48 +0300

    SettingsDropdown remove x

--- src/components/SettingsDropdown.tsx
+++ src/components/SettingsDropdown.tsx
@@ -3,8 +3,7 @@ function SettingsDropdown(props: { children: React.ReactNode; userId?: string })
     const userId = localStorage.getItem("userId");
     const toggleActive = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         const kebab = e.currentTarget;
-        kebab.querySelector(".middle")?.classList.toggle("active");
-        kebab.querySelector(".cross")?.classList.toggle("active");
+        kebab.classList.toggle("active");
         kebab.querySelector(".dropdown")?.classList.toggle("active");
     };
     if (props.userId === userId || localStorage.getItem("isAdmin") === "true")
@@ -12,8 +11,7 @@ function SettingsDropdown(props: { children: React.ReactNode; userId?: string })
             <div className="kebab-container">
                 <div className="kebab" onClick={toggleActive}>
                     <figure></figure>
-                    <figure className="middle"></figure>
-                    <p className="cross">x</p>
+                    <figure></figure>
                     <figure></figure>
                     <ul className="dropdown">{props.children}</ul>
                 </div>
